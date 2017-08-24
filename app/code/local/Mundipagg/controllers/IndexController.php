4401e3d8d2993256035af0b1082183125bd2e1dc
Michel Lima <michelpl@yahoo.com.br>
2017-08-24 18:34:37 -0300

    :bulb: Create first module screen.

---
+++ app/code/local/Mundipagg/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+ 
+class Mundipagg_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Hello developer...';
+    }
+ 
+    public function sayHelloAction()
+    {
+        echo 'Hello one more time...';
+    }
+}
+?>
