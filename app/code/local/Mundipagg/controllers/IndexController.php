d05a6769df96975ec7c6d3055dd45485478ce47d
Michel Lima <michelpl@yahoo.com.br>
2017-08-24 18:37:02 -0300

    :fire: Clean code.

--- app/code/local/Mundipagg/controllers/IndexController.php
+++ app/code/local/Mundipagg/controllers/IndexController.php
@@ -4,12 +4,6 @@ class Mundipagg_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo 'Hello developer...';
-    }
- 
-    public function sayHelloAction()
-    {
-        echo 'Hello one more time...';
     }
 }
 ?>
