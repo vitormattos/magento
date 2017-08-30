03281981817b5b1a7b4b63736c82bcaffc1e16e8
Michel Lima <michelpl@yahoo.com.br>
2017-08-30 19:08:44 -0300

    :heavy_plus_sign: Hide credit card brand options when not used.

---
+++ app/code/community/Mundipagg/Model/CreditCardOperationType.php
@@ -0,0 +1,13 @@
+<?php
+
+class Mundipagg_Model_CreditCardOperationType {
+
+    public function toOptionArray() {
+        return
+            [
+                'AuthAndCapture' => 'Auth and capture',
+                'AuthOnly' => 'Auth only',
+            ];
+    }
+
+}
