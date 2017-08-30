665e4d6b60a0a27f975fc2e115eb92a6b073b1b1
Michel Lima <michelpl@yahoo.com.br>
2017-08-30 18:55:29 -0300

    :heavy_plus_sign: Add all inputs.

---
+++ app/code/community/Mundipagg/Model/BankCodes.php
@@ -0,0 +1,17 @@
+<?php
+
+class Mundipagg_Model_BankCodes {
+
+    public function toOptionArray() {
+        
+        $url = "http://embeddables.eastus2.cloudapp.azure.com/payment/bank_info.json";
+        $banks = json_decode(file_get_contents($url), true);
+        $combo[0] = "Select";
+        if (!empty($banks['banks'])) {
+            foreach ($banks['banks'] as $key => $bank) {
+                $combo[$key] =  $bank['name'];
+            }
+            return $combo;
+        }
+    }
+}
---
+++ app/code/community/Mundipagg/Model/MaxInstallmentsNumber.php
@@ -0,0 +1,23 @@
+<?php
+
+class Mundipagg_Model_MaxInstallmentsNumber {
+
+    public function toOptionArray() {
+        return
+            [
+                '1' => '1x',
+                '2' => '2x',
+                '3' => '3x',
+                '4' => '4x',
+                '5' => '5x',
+                '6' => '6x',
+                '7' => '7x',
+                '8' => '8x',
+                '9' => '9x',
+                '10' => '10x',
+                '11' => '11x',
+                '12' => '12x'
+            ];
+    }
+
+}
